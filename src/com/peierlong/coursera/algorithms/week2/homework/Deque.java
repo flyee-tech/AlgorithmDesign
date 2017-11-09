d72bd34aec7d9eaa460556c85d1396542940d43e
peierlong <peielcn@gmail.com>
2017-11-09 23:42:22 +0800

    链表的双向队列的实现

---
+++ src/com/peierlong/coursera/algorithms/week2/homework/Deque.java
@@ -0,0 +1,135 @@
+package com.peierlong.coursera.algorithms.week2.homework;
+
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * 链表的双向队列的实现
+ * BY: elong
+ * ON: 09/11/2017
+ */
+public class Deque<Item> implements Iterable<Item> {
+
+    private Node<Item> first;
+    private Node<Item> last;
+    private int size;
+
+    public Deque() {
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(Item item) {
+        verifyItem(item);
+        Node<Item> f = first;
+        first = new Node<>(item, null, f);
+        if (f == null) {
+            last = first;
+        } else {
+            f.next = first;
+        }
+        size++;
+    }
+
+    public void addLast(Item item) {
+        verifyItem(item);
+        Node<Item> l = last;
+        last = new Node<>(item, l, null);
+        if (l == null) {
+            first = last;
+        } else {
+            l.prev = last;
+        }
+        size++;
+    }
+
+    public Item removeFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("the deque is empty");
+        }
+        Item item = first.item;
+        first = first.prev;
+        first.next = null;
+        size--;
+        return item;
+    }
+
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("the deque is empty");
+        }
+        Item item = last.item;
+        last = last.next;
+        last.prev = null;
+        size--;
+        return item;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new DequeIterator<>();
+    }
+
+    private void verifyItem(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException("the item is null");
+        }
+    }
+
+    private class Node<E> {
+        private E item;
+        private Node<E> next;
+        private Node<E> prev;
+
+        Node(E item, Node<E> next, Node<E> prev) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private class DequeIterator<E> implements Iterator<E> {
+
+        private Node<Item> current = last;
+        private int i = size;
+
+        @Override
+        public boolean hasNext() {
+            return i > 0;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public E next() {
+            Item item = current.item;
+            current = current.next;
+            i--;
+            return (E) item;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Deque<String> deque = new Deque<>();
+
+        deque.addFirst("a");
+        deque.addFirst("b");
+        deque.addLast("c");
+        deque.addLast("d");
+
+        StdOut.println(deque.removeFirst());
+        StdOut.println(deque.removeLast());
+
+        for (String s : deque) {
+            System.out.println(s);
+        }
+    }
+}
