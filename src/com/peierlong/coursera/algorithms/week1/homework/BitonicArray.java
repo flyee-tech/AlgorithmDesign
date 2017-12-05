ed5e57edfba4a6b173c3fe8dc6f44d5b001e90be
peierlong <peielcn@gmail.com>
2017-12-05 10:28:45 +0800

    week1 homework 双调数组的查找~3lgN实现

---
+++ src/com/peierlong/coursera/algorithms/week1/homework/BitonicArray.java
@@ -0,0 +1,88 @@
+package com.peierlong.coursera.algorithms.week1.homework;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * @author elong
+ * @version V1.0
+ * @date 04/12/2017
+ */
+public class BitonicArray {
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 10, 29, 28, 27, 23, 22, 19, 17};
+        int result = find(a, 6);
+        StdOut.println(result);
+    }
+
+    public static int find(int[] a, int key) {
+        int maxIdx = findTheMaxIdx(a);
+        if (key == a[maxIdx]) {
+            return maxIdx;
+        }
+        int first = findFirst(a, maxIdx, key);
+        if (first != -1) {
+            return first;
+        }
+        return findLast(a, maxIdx, key);
+    }
+
+    /**
+     * Use the binary search find max value
+     * ~lgN
+     */
+    public static int findTheMaxIdx(int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (a[mid] > a[mid + 1]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return hi;
+    }
+
+    /**
+     * Use the binary search find the increasing sequence
+     * ~lgN
+     */
+    private static int findFirst(int[] a, int maxIdx, int key) {
+        int loIdx = 0;
+        int hiIdx = maxIdx;
+        while (loIdx <= hiIdx) {
+            int mid = (loIdx + hiIdx) / 2;
+            if (a[mid] > key) {
+                hiIdx = mid - 1;
+            } else if (a[mid] < key) {
+                loIdx = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Use the binary search find the decreasing sequence
+     * ~lgN
+     */
+    private static int findLast(int[] a, int maxIdx, int key) {
+        int loIdx = maxIdx;
+        int hiIdx = a.length - 1;
+        while (loIdx <= hiIdx) {
+            int mid = (loIdx + hiIdx) / 2;
+            if (a[mid] < key) {
+                hiIdx = mid - 1;
+            } else if (a[mid] > key) {
+                loIdx = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+}
