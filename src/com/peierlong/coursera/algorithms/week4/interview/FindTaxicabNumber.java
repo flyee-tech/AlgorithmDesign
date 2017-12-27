74d069c4ab7ad0935172263c82bdf4b1ced098d5
peierlong <peielcn@gmail.com>
2017-12-27 11:46:38 +0800

    find Taxicab numbers

--- src/com/peierlong/coursera/algorithms/week4/interview/FindTaxicabNumber.java
+++ src/com/peierlong/coursera/algorithms/week4/interview/FindTaxicabNumber.java
@@ -1,5 +1,7 @@
 package com.peierlong.coursera.algorithms.week4.interview;
 
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.ArrayList;
@@ -40,11 +42,76 @@ public class FindTaxicabNumber {
         return numbers.toArray(new Integer[0]);
     }
 
-    public static void main(String[] args) {
-        Integer[] a = findAllTaxicabNumber(4000);
-        for (Integer i : a) {
-            StdOut.println(i);
+
+    public static void findTaxinumber(int N) {
+        MinPQ<Taxicab> candidates = new MinPQ<Taxicab>();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                Taxicab t = new Taxicab(i, j);
+                if (candidates.size() < N) {
+                    candidates.insert(t);
+                }
+                else {
+                    Queue<Taxicab> temp = new Queue<Taxicab>();
+                    Taxicab min = candidates.delMin();
+
+                    while (candidates.min().equals(min)) {
+                        temp.enqueue(candidates.delMin());
+                    }
+
+                    if (!t.equals(min)) {
+                        candidates.insert(t);
+                    } else {
+                        temp.enqueue(t);
+                    }
+
+                    if (!temp.isEmpty()) {
+                        for (Taxicab taxi: temp) {
+                            System.out.println(taxi);
+                        }
+                        System.out.println(min);
+                    }
+                }
+            }
         }
+    }
+
+    static class Taxicab implements Comparable<Taxicab> {
+        int n1;
+        int n2;
+        int cube;
+
+        Taxicab(int n1, int n2) {
+            this.n1 = n1;
+            this.n2 = n2;
+            this.cube = n1 * n1 * n1 + n2 * n2 * n2;
+        }
+
+        @Override
+        public int compareTo(Taxicab that) {
+            if (that.cube > this.cube) return -1;
+            if (that.cube < this.cube) return 1;
+            return 0;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof Taxicab) {
+                if (((Taxicab) o).compareTo(this) == 0)
+                    return true;
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return "number: " + cube + " (" + n1 + ", " + n2 + ")";
+        }
+    }
+
+    public static void main(String[] args) {
+        findTaxinumber(10);
     }
 
 
