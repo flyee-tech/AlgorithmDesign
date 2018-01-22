de4ba7b499688b28bb5ad8cfa386b4c97205b719
peierlong <peielcn@gmail.com>
2018-01-22 14:31:03 +0800

    Board数据结构

--- src/com/peierlong/coursera/algorithms/week4/homework/Board.java
+++ src/com/peierlong/coursera/algorithms/week4/homework/Board.java
@@ -1,37 +1,40 @@
 package com.peierlong.coursera.algorithms.week4.homework;
 
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
- * 一个 board 代表一个 8 puzzle 问题 数据对象
+ * Board对象描述了一个N*N的数据结构
  *
  * @author elong
  * @version V1.0
- * @date 2017/12/29
+ * @date 2018/1/19
  */
 public class Board {
-    private final int N;
-    private final int[][] a;
+    private final int[][] blocks;
 
+    // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-        N = blocks.length;
-        a = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            System.arraycopy(blocks[i], 0, a[i], 0, N);
+        this.blocks = new int[blocks.length][blocks.length];
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks.length; j++) {
+                this.blocks[i][j] = blocks[i][j];
+            }
         }
     }
 
     // board dimension n
     public int dimension() {
-        return N;
+        return blocks.length;
     }
 
     // number of blocks out of place
     public int hamming() {
         int hammingNumber = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (a[i][j] == getGoalVal(i, j) && !isEnd(i, j)) {
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks.length; j++) {
+                if (blocks[i][j] != getGoalVal(i, j) && !isEnd(i, j)) {
                     hammingNumber++;
                 }
             }
@@ -41,27 +44,29 @@ public class Board {
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                int value = a[i][j];
+        int manhattanNumber = 0;
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks.length; j++) {
+                int value = blocks[i][j];
                 if (value != 0 && value != getGoalVal(i, j)) {
-                    int initialX = (value - 1) / N;
-                    int initialY = value - 1 - initialX * N;
-                    int distance = Math.abs(i - initialX)
-                            + Math.abs(j - initialY);
-                    sum += distance;
+                    int ii = (value - 1) / blocks.length;
+                    int jj = value - ii * blocks.length - 1;
+                    int distance = Math.abs(i - ii) + Math.abs(j - jj);
+                    manhattanNumber += distance;
                 }
             }
         }
-        return sum;
+        return manhattanNumber;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (a[i][j] != getGoalVal(i, j)) {
+        if (blocks[blocks.length - 1][blocks.length - 1] != 0) {
+            return false;
+        }
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks.length; j++) {
+                if (blocks[i][j] != getGoalVal(i, j)) {
                     return false;
                 }
             }
@@ -69,42 +74,18 @@ public class Board {
         return true;
     }
 
-
     // a board that is obtained by exchanging any pair of blocks
     public Board twin() {
-        Board board = new Board(a);
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N - 1; j++) {
-                if (a[i][j] != 0 && a[i][j + 1] != 0) {
-                    board.swap(i, j, i, j + 1);
+        Board board = new Board(blocks);
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks.length - 1; j++) {
+                if (board.blocks[i][j] != 0 && board.blocks[i][j + 1] != 0) {
+                    swap(i, j, i, j + 1);
                     return board;
                 }
             }
         }
         return board;
-    }
-
-    private boolean swap(int i, int j, int it, int jt) {
-        if (it < 0 || it >= N || jt < 0 || jt >= N) {
-            return false;
-        }
-        int temp = a[i][j];
-        a[i][j] = a[it][jt];
-        a[it][jt] = temp;
-        return true;
-    }
-
-    // 获取某个位置的目标值
-    private int getGoalVal(int i, int j) {
-        if (isEnd(i, j)) {
-            return 0;
-        }
-        return 1 + i * N + j;
-    }
-
-    // 是否是最后一个位置
-    private boolean isEnd(int i, int j) {
-        return i == N - 1 && j == N - 1;
     }
 
     // does this board equal y?
@@ -117,79 +98,105 @@ public class Board {
         }
         if (other instanceof Board) {
             Board that = (Board) other;
-            return this.N == that.N && aIsEquals(this.a, that.a);
-        }
-        return false;
-    }
-
-    private boolean aIsEquals(int[][] a, int[][] others) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (a[i][j] != others[i][j]) {
-                    return false;
+            if (that.blocks != this.blocks) {
+                return false;
+            }
+            for (int i = 0; i < this.blocks.length; i++) {
+                for (int j = 0; j < this.blocks.length; j++) {
+                    if (this.blocks[i][j] != that.blocks[i][j]) {
+                        return false;
+                    }
                 }
             }
         }
         return true;
     }
 
+    private int getGoalVal(int i, int j) {
+        if (isEnd(i, j)) {
+            return 0;
+        }
+        return i * blocks.length + j + 1;
+    }
+
+    private boolean isEnd(int i, int j) {
+        return i == blocks.length - 1 && j == blocks.length - 1;
+    }
+
+    private boolean swap(int i, int j, int si, int sj) {
+        if (si < 0 || si >= blocks.length || sj < 0 || sj >= blocks.length) {
+            return false;
+        }
+        int swap = blocks[i][j];
+        blocks[i][j] = blocks[si][sj];
+        blocks[si][sj] = swap;
+        return true;
+    }
+
     // all neighboring boards
     public Iterable<Board> neighbors() {
         int i0 = 0, j0 = 0;
-        boolean found = false;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (a[i][j] == 0) {
+        boolean isFind = false;
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks.length; j++) {
+                if (blocks[i][j] == 0) {
                     i0 = i;
                     j0 = j;
-                    found = true;
+                    isFind = true;
                     break;
                 }
             }
-            if (found) {
+            if (isFind) {
                 break;
             }
         }
-        Stack<Board> boards = new Stack<>();
-        Board board = new Board(a);
-        boolean isNeighbor = board.swap(i0, j0, i0 - 1, j0);
-        if (isNeighbor) {
-            boards.push(board);
+        Stack<Board> stack = new Stack<>();
+        Board board = new Board(blocks);
+        boolean isSwap = board.swap(i0, j0, i0 - 1, j0);
+        if (isSwap) {
+            stack.push(board);
         }
-        board = new Board(a);
-        isNeighbor = board.swap(i0, j0, i0, j0 - 1);
-        if (isNeighbor) {
-            boards.push(board);
+        board = new Board(blocks);
+        isSwap = board.swap(i0, j0, i0 + 1, j0);
+        if (isSwap) {
+            stack.push(board);
         }
-        board = new Board(a);
-        isNeighbor = board.swap(i0, j0, i0 + 1, j0);
-        if (isNeighbor) {
-            boards.push(board);
+        board = new Board(blocks);
+        isSwap = board.swap(i0, j0, i0, j0 - 1);
+        if (isSwap) {
+            stack.push(board);
         }
-        board = new Board(a);
-        isNeighbor = board.swap(i0, j0, i0, j0 + 1);
-        if (isNeighbor) {
-            boards.push(board);
+        board = new Board(blocks);
+        isSwap = board.swap(i0, j0, i0, j0 + 1);
+        if (isSwap) {
+            stack.push(board);
         }
-        return boards;
+        return stack;
     }
-
 
     // string representation of this board (in the output format specified below)
     public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(N).append("\n");
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", a[i][j]));
+        StringBuilder sb = new StringBuilder();
+        for (int[] block : blocks) {
+            for (int j = 0; j < blocks.length; j++) {
+                sb.append(block[j]).append(" ");
             }
-            s.append("\n");
+            sb.append("\n");
         }
-        return s.toString();
+        return sb.toString();
     }
 
+    // unit tests (not graded)
     public static void main(String[] args) {
-
+        int[][] is = new int[3][3];
+        int start = 0;
+        for (int i = 0; i < is.length; i++) {
+            for (int j = 0; j < is.length; j++) {
+                is[i][j] = start++;
+            }
+        }
+        Board board = new Board(is);
+        StdOut.println(board);
     }
 
 }
