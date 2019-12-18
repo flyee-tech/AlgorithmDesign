48b1806bc7efbf180c29975b3d344db10e0485f3
peierlong <peielcn@gmail.com>
2019-12-18 23:48:21 +0800

    feat: two sum approach 2

--- src/com/peierlong/leetcode/TwoSum.java
+++ src/com/peierlong/leetcode/TwoSum.java
@@ -1,6 +1,8 @@
 package com.peierlong.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Peiel
@@ -31,9 +33,23 @@ public class TwoSum {
         throw new IllegalArgumentException("no two sum solution");
     }
 
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int n = target - nums[i];
+            if (n > 0 && map.containsKey(n) && i != map.get(n)) {
+                return new int[]{i, map.get(n)};
+            }
+        }
+        throw new IllegalArgumentException("no two sum solution");
+    }
+
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
-        int[] result = twoSum1(nums, 9);
+        int[] result = twoSum2(nums, 9);
 //        System.out.println(Arrays.stream(result).mapToObj(String::valueOf).collect(Collectors.joining(",")));
         System.out.println(Arrays.toString(result));
     }
