4c01c42d29dcc6ea3d2fe0f197bded07eac58785
peierlong <peielcn@gmail.com>
2019-12-30 16:09:17 +0800

    feat: leetcode4: find median sorted arrays

--- src/com/peierlong/leetcode/FindMedianSortedArrays.java
+++ src/com/peierlong/leetcode/FindMedianSortedArrays.java
@@ -39,7 +39,10 @@ public class FindMedianSortedArrays {
         }
 
         System.out.println(Arrays.toString(merge));
+        return getMedian(merge);
+    }
 
+    private static double getMedian(int[] merge) {
         // 执行公式计算中位数
         int len3 = merge.length;
         if (len3 % 2 == 0) {
@@ -49,13 +52,50 @@ public class FindMedianSortedArrays {
         }
     }
 
+    /*
+    leetcode out:
+        Runtime: 2 ms, faster than 99.97% of Java online submissions for Median of Two Sorted Arrays.
+        Memory Usage: 46.9 MB, less than 88.20% of Java online submissions for Median of Two Sorted
+    time complexity:
+        O(log(min(m,n)))
+    space complexity:
+        O(1)
+     */
+    public static double bestSolution(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return bestSolution(nums2, nums1);
+        }
+        int x = nums1.length;
+        int y = nums2.length;
+        int low = 0, high = x;
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+            int maxLeftX = partitionX == 0 ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = partitionX == x ? Integer.MAX_VALUE : nums1[partitionX];
+            int maxLeftY = partitionY == 0 ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = partitionY == y ? Integer.MAX_VALUE : nums2[partitionY];
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((x + y) % 2 == 0) {
+                    return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2d;
+                } else {
+                    return Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+        throw new IllegalArgumentException("not found");
+    }
+
     public static void main(String[] args) {
 
-        int[] nums1 = {1, 2};
-        int[] nums2 = {3, 4};
+        int[] nums1 = {2};
+        int[] nums2 = {};
 
-
-        double d = solution1(nums1, nums2);
+        double d = bestSolution(nums1, nums2);
         System.out.println(d);
 
     }
