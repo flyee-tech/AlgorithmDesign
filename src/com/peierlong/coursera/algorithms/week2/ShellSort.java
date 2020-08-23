52d5240dff7d4dcbf3c37fa626016c4fa6c43133
peiel <peielcn@gmail.com>
2020-08-23 13:44:34 +0800

    refactor: 希尔排序的实现

--- src/com/peierlong/coursera/algorithms/week2/ShellSort.java
+++ src/com/peierlong/coursera/algorithms/week2/ShellSort.java
@@ -19,17 +19,13 @@ public class ShellSort {
     public static void sort(Comparable[] a) {
         int N = a.length;
         int h = 1;
-        for (; h < N / 3; ) {
-            h = 3 * h + 1;
+        while (h < N / 3) {
+            h = h * 3 + 1;
         }
-        for (; h >= 1; ) {
+        while (h >= 1) {
             for (int i = h; i < N; i++) {
-                for (int j = i; j - h >= 0; j -= h) {
-                    if (less(a[j], a[j - h])) {
-                        swap(a, j, j - h);
-                    } else {
-                        break;
-                    }
+                for (int j = i; j >= h && less(a[j], a[j - h]); j -= h) {
+                    swap(a, j, j - h);
                 }
             }
             h = h / 3;
