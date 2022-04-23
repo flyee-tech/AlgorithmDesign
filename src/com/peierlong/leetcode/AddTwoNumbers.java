009dd8252fa6d3c952a4feb8fbddd09e55e34966
Erick <erick.pei@qq.com>
2022-04-23 14:53:32 +0800

    feat: LeetCode002完善注释

--- src/com/peierlong/leetcode/AddTwoNumbers.java
+++ src/com/peierlong/leetcode/AddTwoNumbers.java
@@ -18,17 +18,26 @@ public class AddTwoNumbers {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 初始化一个头结点
         ListNode node = new ListNode(0);
+        // 初始化一个指针，指向头结点，为了后面操作
         ListNode result = node;
+        // 初始化一个进位，相加大于10的时候，进位为1
         int d = 0;
+        // 当两个链表都不为空或者存在进位的时候，继续操作
         while (l1 != null || l2 != null || d == 1) {
+            // 执行相加操作
             int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + d;
+            // 计算进位
             d = sum / 10;
+            // 将结果赋值给当前结点的下个节点
             node.next = new ListNode(sum % 10);
+            // 将当前结点后移
             node = node.next;
             l1 = l1 != null ? l1.next : null;
             l2 = l2 != null ? l2.next : null;
         }
+        // 返回结果，去掉头结点
         return result.next;
     }
 
