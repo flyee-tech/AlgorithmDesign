33578dedcd346a3d2e80737bc0ac4db07840d769
Peiel <peielcn@gmail.com>
2022-10-29 15:24:29 +0800

    add LC011ContainerWithMostWater.java

---
+++ src/com/peierlong/leetcode/v2/LC011ContainerWithMostWater.java
@@ -0,0 +1,39 @@
+package com.peierlong.leetcode.v2;
+
+public class LC011ContainerWithMostWater {
+
+    public static int maxArea001(int[] height) {
+        int max = 0;
+        int n = height.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int minLines = Math.min(height[i], height[j]);
+                max = Math.max(max, minLines * (j - i));
+            }
+        }
+        return max;
+    }
+
+    public static int maxArea(int[] height) {
+        int max = 0;
+        int left = 0, right = height.length - 1;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            max = Math.max(max, area);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int max1 = maxArea(new int[]{1, 1});
+        System.out.println(max1);
+        int max2 = maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
+        System.out.println(max2);
+    }
+
+}
